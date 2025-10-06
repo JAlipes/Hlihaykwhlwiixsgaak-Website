cf7d52369cf5c3d21a3fabd7b33a23c50f999673
coDingcaRot <jonathanielalipes905@gmail.com>
2025-10-06 15:59:25 -0700

    Refactored text and image to be editable, Added hidden save button

--- frontend/src/components/AboutMelanieMark.tsx
+++ frontend/src/components/AboutMelanieMark.tsx
@@ -1,17 +1,69 @@
+import { useState, useContext, useEffect } from 'react';
+
+// Import Assets
 import headshot from '../assets/MJM_FINAL_HEADSHOT_2024_BLUE_WHITE.jpg';
 
+// Import Context
+import { AuthContext } from '../contexts/AuthContext';
+
+// Import Utils
+import { HandleSaveSectionData } from '../utils/HandleSaveSectionData';
+import { HandleGetSectionData } from '../utils/HandleGetSectionData';
+import { HandleImageChangeFactory } from '../utils/HandleImageChangeFactory';
+
+// Import Components
+import EditableImage from './EditableImage';
+import EditableText from './EditableText';
+import SaveEditsButton from './SaveEditsButton';
+
 export default function AboutMelanieMark() {
+    const sectionName: string = `about`;
+    const { isAuthenticated } = useContext(AuthContext);
+
+    const [aboutText, setAboutText] = useState<string>(
+    `I’m Melanie Mark, a proud mother of two daughters and descendant of the Nisga’a, Gitxsan, Cree, and Ojibway First Nations. Born and raised in East Vancouver, I’ve lived and worked throughout B.C. and Canada, cultivating meaningful relationships in various sectors. My late grandparents are from Laxgalts’ap, Gitanmaax and Peguis First Nations and attended St. Michael’s, Elkhorn and Brandon Indian Residential Schools.
+    
+Over the past three decades, I’ve served as an advocate, public servant, elected official, and now entrepreneur in the cleantech space. My journey has been guided by a commitment to justice, empowerment, and systemic change.
+
+As the first First Nations woman elected to the B.C. Legislature and appointed to Cabinet, I’ve had the privilege of working with diverse communities, from grassroots organizations to executive leadership. Now, as the founder of Hli Haykwhl Ẃii X̲sgaak Consulting, I’m sharing my knowledge and experiences to help others achieve their goals through the power of culture, sport, education, and industry.
+
+CEO, Melanie Mark,
+Hli Haykwhl Ẃii X̲sgaak Consulting`
+    );
+    const [ aboutImage, setAboutImage ] = useState<string>(headshot);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+     useEffect(() => {
+        const fetchSectionData = async () => {
+            const data = await HandleGetSectionData(sectionName);
+            if (data) {
+                setAboutText(data.text);
+                setAboutImage(data.image);
+            }
+        };
+        fetchSectionData();
+    }, []);
+
+    const handleAboutImageChange = HandleImageChangeFactory(setSelectedFile, setAboutImage);
+
+
     return (
-        <section className="bg-gray-100 text-gray-800 py-16 px-6 md:px-20 lg:px-32">
+        <section className="h-auto bg-white text-gray-800 py-16 px-6 md:px-20 lg:px-32">
             <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                 {/* Text (left) */}
                 <div>
                     <h2 className="text-3xl md:text-4xl font-bold text-red-600 text-center">
                         About Melanie Mark
                     </h2>
-                    <div className="w-48 h-[2px] bg-black mt-3 mb-8 mx-auto" />
+                    <div className="w-48 h-[2px] bg-black mt-3 mb-8 mx-auto" /> {/*Underline*/}
 
-                    <p className="text-lg leading-relaxed mb-4">
+                    <EditableText
+                        isAuthenticated={isAuthenticated}
+                        setText={setAboutText}
+                        text={aboutText}
+                    />
+
+                    {/* <p className="text-lg leading-relaxed mb-4">
                         I’m Melanie Mark, a proud mother of two daughters and decendent of
                         the Nisga’a, Gitxsan, Cree, and Ojibway First Nations. Born and
                         raised in East Vancouver, I’ve lived and worked throughout B.C. and
@@ -41,17 +93,30 @@ export default function AboutMelanieMark() {
                     <p className="text-lg font-semibold mt-6 text-right">
                         CEO, Melanie Mark,
                         <br /> Hli Haykwhl Ẃii Xsgaak Consulting
-                    </p>
+                    </p> */}
                 </div>
 
                 {/* Image (right) */}
-                <div className="flex justify-center md:justify-end">
-                    <img
-                        src={headshot}
-                        alt="Melanie Mark standing with Canadian and Indigenous flags"
-                        className="w-full max-w-[500px] rounded-lg shadow-lg object-cover"
+                <EditableImage 
+                    src={aboutImage}
+                    alt='Melanie Mark standing with Canadian and Indigenous flags'
+                    wrapperClassName='flex justify-center md:justify-end'
+                    imageClassName='w-full max-w-[500px] rounded-lg shadow-lg object-cover cursor-pointer'
+                    isAuthenticated={isAuthenticated}
+                    inputIdString='aboutImageUpload'
+                    onChangeFunction={handleAboutImageChange}
+                />
+
+                {/* Save button shows only if authenticated */}
+                {isAuthenticated && (
+                    <SaveEditsButton 
+                        onClickFunction={
+                            () => {
+                                HandleSaveSectionData({sectionName, text: aboutText, image: selectedFile ?? aboutImage});
+                            }
+                        }
                     />
-                </div>
+                )}            
             </div>
         </section>
     );
